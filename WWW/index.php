fa471b00100a94b3002532fb695d9d2e94459eea
Shamuratov222 <d.schamuratov2014@yandex.ru>
2021-12-08 20:15:05 +0500

    Добавление файла главной страницы

---
+++ WWW/index.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Shamuratov Damir</title></head>
+</html>
+<p> Data i vremya:
+<p>
+<ul>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/index.php">Main</a></li>
+
+</ul>
+<?php
+$d=date("d.m.Y H:i");
+echo($d);
+?>
