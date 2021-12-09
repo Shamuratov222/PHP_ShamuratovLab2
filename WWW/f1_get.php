ab35e81c138d22502a753d2e46784486b0306afe
Shamuratov222 <d.schamuratov2014@yandex.ru>
2021-12-09 16:34:18 +0500

    Выполнение задания 3-1 лабораторной работы 3 и изменение главной страницы

---
+++ WWW/f1_get.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Shamuratov Damir</title></head>
+</html>
+<?php
+echo ("Hello, " . $_GET["userName"]);
+echo ("<hr>");
+echo ("hideField is " .
+      $_GET["hideField"]);
+?>
--- WWW/index.php
+++ WWW/index.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>Shamuratov Damir</title></head>
 </html>
-<p> Data i vremya:
+<p> Laba 2:
 <p>
 <ul>
 <li><a href="/denwer/PHP_ShamuratovLab2/WWW/index.php">Main</a></li>
@@ -15,6 +15,22 @@
 <li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-6.php">Zadanie 2-6</a></li>
 <li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-6.2.php">Zadanie 2-6.2</a></li>
 </ul>
+<p> Laba 3:
+<p>
+<ul>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/index.php">Main</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-1.php">Zadanie 2-1</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-2.php">Zadanie 2-2</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-3.php">Zadanie 2-3</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-4.php">Zadanie 2-4</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-4.2.php">Zadanie 2-4.2</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-5.php">Zadanie 2-5</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-5.2.php">Zadanie 2-5.2</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-6.php">Zadanie 2-6</a></li>
+<li><a href="/denwer/PHP_ShamuratovLab2/WWW/lab-2-6.2.php">Zadanie 2-6.2</a></li>
+</ul>
+<p> Data i vremya:
+<p>
 <?php
 $d=date("d.m.Y H:i");
 echo($d);
