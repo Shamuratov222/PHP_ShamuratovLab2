a3767e289c7e30ad7aab025a7e6f317e65374799
Shamuratov222 <d.schamuratov2014@yandex.ru>
2021-12-09 16:42:02 +0500

    Выполнение задания 3-2 из лр3

---
+++ WWW/f2_post.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Shamuratov Damir</title></head>
+</html>
+<?php
+echo ("Hello, " . $_POST["userName_post"]);
+echo ("<hr>");
+echo ("hideField_post is " .
+$_POST["hideField_post"]);
+?>
